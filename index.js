f20f01a88a9809564ff81ac4768742bc710418f1
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2022-03-15 02:27:50 +0530

    added files

---
+++ index.js
@@ -0,0 +1,9 @@
+var imgDo = document.getElementById('kit-1');
+var btn = document.getElementById('ak-2');
+var getS = document.getElementById('near-1');
+btn.addEventListener('click', () => {
+    getS.style.display = "flex";
+    document.body.style.backgroundColor = "#242424";
+    imgDo.src = '/svg/ul-1.svg';
+
+})
